fb704676413d8bb40b29c1d0dc33747b0c82a5fa
AugustT <tomaug@ceh.ac.uk>
2014-09-17 11:32:07 +0100

    update getData.r

--- getData.r
+++ getData.r
@@ -2,10 +2,17 @@
 library(rnbn)
 
 nbnLogin(username = 'tom_test', password = 'HelloWorld')
-sp <- getTVKQuery(query = 'Bombus hypnorum', top = TRUE)
-Bhyp <- getOccurrences(tvks = sp$ptaxonVersionKey)
-
-str(Bhyp)
+sp1 <- getTVKQuery(query = 'Bombus hypnorum', top = TRUE)
+Bhyp <- getOccurrences(tvks = sp1$ptaxonVersionKey)
 
 save(Bhyp, file = 'Bhyp.rdata')
 
+## Get data for Bats
+UK_bats <- c('Alcathoe bat', 'barbastelle', "Bechstein's bat", "Brandt's bat", "brown long-eared bat",
+             "common pipistrelle", "Daubenton's bat", "greater horseshoe bat", "grey long-eared bat",
+             "Leisler's bat", "lesser horseshoe bat", "Nathusius' pipistrelle", "Natterer's bat",
+             "noctule", "serotine", "soprano pipistrelle","whiskered bat")
+sp2 <- getTVKQuery(query = UK_bats, species_only = TRUE, top = TRUE)
+Chiro <- getOccurrences(tvks = sp2$ptaxonVersionKey)
+
+save(Chiro, file = 'Chiro.rdata')
